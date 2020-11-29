ff21b7221e48d47d36bdf520134fb08238e75f5f
Daniel <50612327+daniel2IT@users.noreply.github.com>
2020-11-29 17:32:42 +0200

    Rsa_Algorithm

---
+++ RSA algorithm/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace RSA_algorithm
+{
+    partial class RSA_Algorithm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RSA_Algorithm));
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox_p = new System.Windows.Forms.TextBox();
+            this.textBox_q = new System.Windows.Forms.TextBox();
+            this.EnryptionBTN = new System.Windows.Forms.Button();
+            this.DecryptionBTN = new System.Windows.Forms.Button();
+            this.textiFaila = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog3save = new System.Windows.Forms.OpenFileDialog();
+            this.textBox_d = new System.Windows.Forms.TextBox();
+            this.textBox_n = new System.Windows.Forms.TextBox();
+            this.viesasisRaktas = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.label3.Location = new System.Drawing.Point(85, 137);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "p = ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.label4.Location = new System.Drawing.Point(271, 137);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 25);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "q =";
+            // 
+            // textBox_p
+            // 
+            this.textBox_p.Location = new System.Drawing.Point(140, 137);
+            this.textBox_p.Multiline = true;
+            this.textBox_p.Name = "textBox_p";
+            this.textBox_p.Size = new System.Drawing.Size(54, 45);
+            this.textBox_p.TabIndex = 8;
+            this.textBox_p.Text = "23";
+            // 
+            // textBox_q
+            // 
+            this.textBox_q.Location = new System.Drawing.Point(320, 137);
+            this.textBox_q.Multiline = true;
+            this.textBox_q.Name = "textBox_q";
+            this.textBox_q.Size = new System.Drawing.Size(54, 45);
+            this.textBox_q.TabIndex = 9;
+            this.textBox_q.Text = "31";
+            // 
+            // EnryptionBTN
+            // 
+            this.EnryptionBTN.BackColor = System.Drawing.Color.Lime;
+            this.EnryptionBTN.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.EnryptionBTN.Font = new System.Drawing.Font("Microsoft YaHei", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EnryptionBTN.ForeColor = System.Drawing.Color.Black;
+            this.EnryptionBTN.Location = new System.Drawing.Point(12, 24);
+            this.EnryptionBTN.Name = "EnryptionBTN";
+            this.EnryptionBTN.Size = new System.Drawing.Size(236, 107);
+            this.EnryptionBTN.TabIndex = 10;
+            this.EnryptionBTN.Text = "Encryption";
+            this.EnryptionBTN.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.EnryptionBTN.UseVisualStyleBackColor = false;
+            this.EnryptionBTN.Click += new System.EventHandler(this.EnryptionBTN_Click);
+            // 
+            // DecryptionBTN
+            // 
+            this.DecryptionBTN.BackColor = System.Drawing.Color.Red;
+            this.DecryptionBTN.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.DecryptionBTN.Font = new System.Drawing.Font("Microsoft YaHei", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DecryptionBTN.ForeColor = System.Drawing.Color.Black;
+            this.DecryptionBTN.Location = new System.Drawing.Point(281, 24);
+            this.DecryptionBTN.Name = "DecryptionBTN";
+            this.DecryptionBTN.Size = new System.Drawing.Size(217, 107);
+            this.DecryptionBTN.TabIndex = 11;
+            this.DecryptionBTN.Text = "Decryption";
+            this.DecryptionBTN.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.DecryptionBTN.UseVisualStyleBackColor = false;
+            this.DecryptionBTN.Click += new System.EventHandler(this.DecryptionBTN_Click);
+            // 
+            // textiFaila
+            // 
+            this.textiFaila.Location = new System.Drawing.Point(12, 188);
+            this.textiFaila.Multiline = true;
+            this.textiFaila.Name = "textiFaila";
+            this.textiFaila.Size = new System.Drawing.Size(486, 220);
+            this.textiFaila.TabIndex = 12;
+            this.textiFaila.Text = resources.GetString("textiFaila.Text");
+            // 
+            // folderBrowserDialog3save
+            // 
+            this.folderBrowserDialog3save.FileName = "folderBrowserDialog3save";
+            // 
+            // textBox_d
+            // 
+            this.textBox_d.Location = new System.Drawing.Point(444, 363);
+            this.textBox_d.Multiline = true;
+            this.textBox_d.Name = "textBox_d";
+            this.textBox_d.Size = new System.Drawing.Size(54, 45);
+            this.textBox_d.TabIndex = 14;
+            this.textBox_d.Visible = false;
+            // 
+            // textBox_n
+            // 
+            this.textBox_n.Location = new System.Drawing.Point(444, 188);
+            this.textBox_n.Multiline = true;
+            this.textBox_n.Name = "textBox_n";
+            this.textBox_n.Size = new System.Drawing.Size(54, 45);
+            this.textBox_n.TabIndex = 15;
+            this.textBox_n.Visible = false;
+            // 
+            // viesasisRaktas
+            // 
+            this.viesasisRaktas.Location = new System.Drawing.Point(444, 229);
+            this.viesasisRaktas.Multiline = true;
+            this.viesasisRaktas.Name = "viesasisRaktas";
+            this.viesasisRaktas.Size = new System.Drawing.Size(54, 137);
+            this.viesasisRaktas.TabIndex = 18;
+            this.viesasisRaktas.Visible = false;
+            // 
+            // RSA_Algorithm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Beige;
+            this.ClientSize = new System.Drawing.Size(511, 423);
+            this.Controls.Add(this.viesasisRaktas);
+            this.Controls.Add(this.textBox_n);
+            this.Controls.Add(this.textBox_d);
+            this.Controls.Add(this.textiFaila);
+            this.Controls.Add(this.DecryptionBTN);
+            this.Controls.Add(this.EnryptionBTN);
+            this.Controls.Add(this.textBox_q);
+            this.Controls.Add(this.textBox_p);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.ForeColor = System.Drawing.Color.DarkCyan;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.IsMdiContainer = true;
+            this.Name = "RSA_Algorithm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "RSA Algorithm";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox_p;
+        private System.Windows.Forms.TextBox textBox_q;
+        private System.Windows.Forms.Button EnryptionBTN;
+        private System.Windows.Forms.Button DecryptionBTN;
+        private System.Windows.Forms.TextBox textiFaila;
+        private System.Windows.Forms.OpenFileDialog folderBrowserDialog3save;
+        private System.Windows.Forms.TextBox textBox_d;
+        private System.Windows.Forms.TextBox textBox_n;
+        private System.Windows.Forms.TextBox viesasisRaktas;
+    }
+}
+
---
+++ RSA algorithm/Form1.cs
@@ -0,0 +1,294 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO; // for files/folder
+using NUnit.Framework;
+using System.Numerics;
+using Xunit;
+using System.Diagnostics;
+
+namespace RSA_algorithm
+{
+    public partial class RSA_Algorithm : Form
+    {
+        public RSA_Algorithm()
+        {
+            // p minimum 11 , q minimum 13, kad butu tas chas 97 ir > 
+
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        // Patikrinimas ar Key yra paprastas
+        private bool IsTheNumberSimple(BigInteger n)
+        {
+            if (n < 2) return false;
+
+            if (n == 2) return true;
+
+            for (long i = 2; i < n; i++)
+                if (n % i == 0) return false;
+
+            return true;
+        }
+
+        // d radimas bendro dalyklio kad butu = 1 (d turi buti taip pat paprastam su m).
+        private BigInteger Calculate_DBD(BigInteger m)
+        {
+            BigInteger d = m - 1; // kandidatai 
+
+            for (BigInteger i = 2; i <= m; i++) // i - 1 is karto ismetam ... 
+                if ((m % i == 0) && (d % i == 0)) //jeigu jie turi tokius pacius dalyklius
+                {
+                    d--;
+                    i = 1;
+                }
+
+            return d; //19
+
+        }
+
+        // Privatus Raktas
+        private BigInteger Calculate_d(BigInteger d, BigInteger m) // d ir Fn 
+        {
+            long e = 10; // yra 10 isrenkam dalyklius 1,2,5,10
+
+            while (true)
+            {
+                if ((e * d) % m == 1) //Fn kaip Moda == 1
+                    break;
+                else e++;
+            }
+
+            return e;
+        }
+
+        // Metodas Atliekantis Desifrvima eiluciu
+        private string RSA_Dedoce(List<string> input, BigInteger d, BigInteger n)
+        {
+
+            string result = "";
+
+            BigInteger bi;
+            int c = 0;
+
+            foreach (string item in input)
+            {
+                bi = new BigInteger(Convert.ToDouble(item));
+                bi = BigInteger.Pow(bi, (int)d);
+
+                BigInteger n_ = new BigInteger((int)n);
+
+                bi = bi % n_;
+
+                //int index = item[i] + 97;d
+                //  i++;
+                //  result += characters[convertedIndex].ToString();
+
+                //  int index = Convert.ToInt32(bi.ToString());
+
+                //result += characters[index].ToString();
+
+                c = (int)bi;
+
+                result += Convert.ToChar(c);
+            }
+            return result;
+        }
+
+        // Metodas kuris atlieka eiluciu sifravima
+        private List<string> RSA_Endoce(string s, BigInteger e, BigInteger n)
+        {
+            List<string> result = new List<string>();
+
+            BigInteger bi;
+
+            char[] charText = s.ToCharArray();
+            int[] intText = new int[charText.Length];
+
+            for (int i = 0; i < charText.Length; i++)
+            {
+
+                intText[i] = charText[i];
+            }
+
+            for (int i = 0; i < s.Length; i++)
+            {
+
+                int index = Array.IndexOf(charText, s[i]);
+
+                bi = new BigInteger(intText[i]);
+                bi = BigInteger.Pow(bi, (int)e);
+
+                BigInteger n_ = new BigInteger((int)n);
+
+                bi = bi % n_;
+
+                result.Add(bi.ToString());
+            }
+
+            return result;
+        }
+
+        public BigInteger musuP;
+        public BigInteger musuQ;
+        private (BigInteger, BigInteger) PirminiuRadimas()
+        {
+
+            long n = Convert.ToInt64(textBox_n.Text); // musu n 
+
+            for (BigInteger prime1 = 0; prime1 < n; prime1++)
+            {
+
+                for (BigInteger prime2 = 0; prime2 < n; prime2++)
+                {
+
+                    if (prime1 * prime2 == n)
+                    {
+                        musuP = prime1;
+                        musuQ = prime2;
+                    }
+
+                }
+            }
+            return (musuP, musuQ);
+
+        }
+
+        /*     char[] characters = new char[] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+                                                    'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+                                                    'T', 'U', 'V', 'W', 'X', 'Y', 'Z', ' ', '1', '2', '3', '4', '5', '6', '7',
+                                                    '8', '9', '0' } - NOT SECURE OPTION;*/
+
+        public static void DeleteLastLine(string filepath)
+        {
+            List<string> lines = File.ReadAllLines(filepath).ToList();
+
+            File.WriteAllLines(filepath, lines.GetRange(0, lines.Count - 1).ToArray());
+
+        }
+
+        private void DecryptionBTN_Click(object sender, EventArgs e)
+        {
+            if ((textBox_d.Text.Length > 0) && (textBox_n.Text.Length > 0))
+            {
+                //pirmo line issirinkimas
+                BigInteger n = BigInteger.Parse(File.ReadLines("out1.txt").First());
+                // deleting 1 line
+                var SkipN = File.ReadAllLines("out1.txt").Skip(1);
+                File.WriteAllLines("out1.txt", SkipN);
+                //Last line issirinkimas
+                BigInteger DBD = BigInteger.Parse(File.ReadLines("out1.txt").Last());
+                // deleting last line
+                DeleteLastLine("out1.txt");
+
+                List<string> input = new List<string>();
+                /////////////////////////P ir Q radimas/////////////////////////
+                BigInteger p;
+                BigInteger q;
+                var resultt = PirminiuRadimas();
+                q = resultt.Item1;
+                p = resultt.Item2;
+                /////////////////////////////////////////////////////////////////
+                ////////////////////////////////////////////////////////////////////
+                /////////////////////////Privacio Rakto Radimas///////////////////////////////
+                BigInteger Fn = (p - 1) * (q - 1);
+
+                BigInteger privateKey = Calculate_d(DBD, Fn); // Privatus Raktas
+
+                StreamReader sr = new StreamReader("out1.txt");
+
+                while (!sr.EndOfStream)
+                {
+                    input.Add(sr.ReadLine());
+
+                }
+                //  foreach(string a in input)
+                //   {
+                //  MessageBox.Show("a = " + a); 
+                //   }
+
+                sr.Close();
+
+                string result = RSA_Dedoce(input, privateKey, n);
+                //MessageBox.Show("RESULT  = " + result);
+                StreamWriter sw = new StreamWriter("out2.txt");
+                sw.Write(result); //Line
+                sw.Close();
+
+                Process.Start("out2.txt");
+            }
+            else MessageBox.Show("Type Key!");
+        }
+
+        private void EnryptionBTN_Click(object sender, EventArgs e)
+        {
+            if ((textBox_p.Text.Length > 0) && (textBox_q.Text.Length > 0))
+            {
+                BigInteger p = Convert.ToInt64(textBox_p.Text);
+                BigInteger q = Convert.ToInt64(textBox_q.Text);
+
+                if (IsTheNumberSimple(p) && IsTheNumberSimple(q))
+                {
+                    string s = "";
+
+                    StreamWriter swin = new StreamWriter("in.txt");
+
+                    swin.WriteLine(textiFaila.Text); //Line
+
+                    swin.Close();
+                    StreamReader sr = new StreamReader("in.txt");
+
+                    while (!sr.EndOfStream)
+                    {
+                        s += sr.ReadLine();
+                    }
+
+                    sr.Close();
+
+                    //    s = s.ToUpper();
+
+                    BigInteger n = p * q;
+                    BigInteger Fn = (p - 1) * (q - 1);
+                    BigInteger DBD = Calculate_DBD(Fn); // Viesasis Raktas
+                    BigInteger d = Calculate_d(DBD, Fn); // Privatus Raktas
+
+                    List<string> result = RSA_Endoce(s, d, n);
+
+                    StreamWriter sw = new StreamWriter("out1.txt");
+
+                    // n is first line
+                    sw.WriteLine(n);
+                    foreach (string item in result)
+                    {
+                        sw.WriteLine(item);
+                    }
+                    //DBD bus last line faile
+                    sw.WriteLine(DBD);
+
+                    sw.Close();
+                    viesasisRaktas.Text = DBD.ToString();
+                    textBox_d.Text = d.ToString();
+                    textBox_n.Text = n.ToString();
+
+                    Process.Start("out1.txt");
+                }
+                else MessageBox.Show("p ar q - make sure that they're prime numbers!");
+            }
+            else MessageBox.Show("Type p and q > than 20 !");
+
+        }
+
+    }
+}
